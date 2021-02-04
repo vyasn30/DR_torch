6f1acdbaf5ae30223e60426bc1e3a970fca45eac
vyasn30 <vyasn30@outlook.com>
2021-02-04 14:43:19 +0530

    Basic eda

--- eda.py
+++ eda.py
@@ -1,2 +1,12 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+train_df = pd.read_csv("data/train.csv")
+test_df = pd.read_csv("data/test.csv")
+
+plot = train_df['diagnosis'].hist()
 
 
+plt.show()
+print(train_df['diagnosis'].value_counts())
